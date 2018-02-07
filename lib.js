814917089e1e2a7342595b702d64a2cf6523713e
Chris Troutner <chris.troutner@gmail.com>
2018-02-07 09:08:19 -0800

    testing ping

--- lib.js
+++ lib.js
@@ -12,12 +12,14 @@ function sleep(ms) {
 }
 
 async function pingInternet() {
-  const hosts = ["p2pvps.org", "google.com", "yahoo.com"];
+  const hosts = ["p2pvps.org", "google.com", "yahoo.com", "asdahrhejah.com"];
 
   let res;
   for (let i = 0; i < hosts.length; i++) {
     res = await ping.promise.probe(hosts[i]);
-    console.log(res);
+    //console.log(res);
+    if (res.alive) console.log(`${res.host} ping returned successfully.`);
+    else console.log(`${res.host} ping failed.`);
   }
 }
 
