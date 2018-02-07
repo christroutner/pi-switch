5614fc00809f99b4eb4e57209e609c315c0dd18d
Chris Troutner <chris.troutner@gmail.com>
2018-02-07 09:46:06 -0800

    testing relay control

--- lib.js
+++ lib.js
@@ -77,11 +77,17 @@ function toggleRelay1() {
 
   if (toggleVal) {
     console.log(`Turning relay 1 off.`);
-    relay1.writeSync(0);
+    relay1.write(0, funcion(err, value) {
+      if(err) console.error(err);
+      console.log(`Relay 1 turned off with ${value}`);
+    });
     toggleVal = false;
   } else {
     console.log(`Turning relay 1 on.`);
-    relay1.writeSync(1);
+    relay1.writeSync(1, function(err, value) {
+      if(err) console.error(err);
+      console.log(`Relay 1 turned on with ${value}`);
+    });
     toggleVal = true;
   }
 }
